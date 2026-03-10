4c155c1ea6725f6c1c831d376fd524d3f3be4313
xyrjok <yyl9821@163.com>
2026-03-10 10:21:12 +0800

    Create index.vue

---
+++ src/layout/aside/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside class="aside">
+    <ul>
+      <li>仪表盘</li>
+      <li>业务管理</li>
+      <li>系统设置</li>
+    </ul>
+  </aside>
+</template>
+<style scoped>
+.aside { width: 200px; background: #324157; color: #bfcbd9; }
+.aside ul { list-style: none; padding: 0; margin: 0; }
+.aside li { padding: 15px 20px; cursor: pointer; border-bottom: 1px solid #283446; }
+.aside li:hover { background: #242f42; color: #20a0ff; }
+</style>
